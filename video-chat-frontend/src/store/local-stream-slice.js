e11aa074aee2057dd9d1e964de0f2f8e28d22485
Prerona <preronaghosh1997@gmail.com>
2023-10-15 12:55:20 -0400

    Added call states

--- video-chat-frontend/src/store/local-stream-slice.js
+++ video-chat-frontend/src/store/local-stream-slice.js
@@ -1,11 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// Store the different possible call states
+const callStates = {
+    'Unvailable' : 0,
+    'Available' : 1,
+    'Requested' : 2,
+    'InProgress' : 3
+};
+
 const localStreamSlice = createSlice({
     name : 'localStream',
-    initialState: { localStream : null },
+    initialState: { localStream : null, callState : callStates.Unvailable },
     reducers: {
         setLocalStream(state, action) {
             state.localStream = action.payload
+        },
+        setCallState(state, action) {
+            state.callState = action.payload;
         }
     }    
 });
--- video-chat-frontend/src/utils/webRtc/webRtcHandler.js
+++ video-chat-frontend/src/utils/webRtc/webRtcHandler.js
@@ -1,6 +1,13 @@
 import store from "../../store/index";
 import { localSteamActions } from "../../store/local-stream-slice";
 
+const callStates = {
+    'Unvailable' : 0,
+    'Available' : 1,
+    'Requested' : 2,
+    'InProgress' : 3
+};
+
 const defaultConstraints = {
     video: true,
     audio: true
@@ -10,6 +17,10 @@ export const getLocalStream = async () => {
     try {
         const stream = await navigator.mediaDevices.getUserMedia(defaultConstraints); // this returns a promise and that resolves to a MediaStream object
         store.dispatch(localSteamActions.setLocalStream(stream));
+
+        // After receiving the localStream successfully, we can update the call state
+        store.dispatch(localSteamActions.setCallState(callStates.Available));
+        
     } catch (error) {
         console.log("Error occurred while trying to get access to localstream");
         console.log(error);
