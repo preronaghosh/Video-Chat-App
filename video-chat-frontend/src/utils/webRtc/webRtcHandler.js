1ccc5877d9a1c103e6c20f1ee3f2c6ec389e37ae
Prerona <preronaghosh1997@gmail.com>
2023-10-16 21:05:14 -0400

    logic for creating peer connection added

--- video-chat-frontend/src/utils/webRtc/webRtcHandler.js
+++ video-chat-frontend/src/utils/webRtc/webRtcHandler.js
@@ -3,10 +3,17 @@ import { localStreamActions, callStates } from "../../store/local-stream-slice";
 import { sendPreOffer, sendPreOfferAnswer } from "../wssConnection/wssConnection";
 
 let connectedUserSocketId; // Stores currently connected user on a direct call
+let peerConnection; 
 
 const defaultConstraints = {   // Stores the default mediaStream object config
     video: true,
     audio: true
+};
+
+const peerConnectionConfig = {
+    iceServers: [{ 
+        urls: 'stun:stun.l.google.com:13902'
+    }]
 };
 
 const preOfferAnswers = {
@@ -37,6 +44,8 @@ export const getLocalStream = async () => {
 
         // After receiving the localStream successfully, we can update the call state
         store.dispatch(localStreamActions.setCallState(callStates.Available));
+
+        createPeerConnection();
         
     } catch (error) {
         console.log("Error occurred while trying to get access to localstream");
@@ -61,8 +70,8 @@ export const callAnotherUser = (calleeDetails) => {
     });
 }; 
 
-// Method to handle when another user is sending a pre-offer request to current user
-export const handleIncomingPreOffer = (data) => {
+// Methods for handling pre-offer requests and responses
+export const handleIncomingPreOffer = (data) => {   // Method to handle when another user is sending a pre-offer request to current user
 
     if (isCallPossible()) {
         connectedUserSocketId = data.callerSocketId;
@@ -78,22 +87,6 @@ export const handleIncomingPreOffer = (data) => {
 
     }
 };
-
-export const acceptIncomingCallRequest = () => {
-    sendPreOfferAnswer({
-        callerSocketId: connectedUserSocketId, 
-        answer: preOfferAnswers.Accepted
-    });
-}; 
-
-export const rejectIncomingCallRequest = () => {
-    sendPreOfferAnswer({
-        callerSocketId: connectedUserSocketId,
-        answer: preOfferAnswers.Rejected
-    });
-
-    resetCallData();
-}; 
 
 export const handleIncomingPreOfferAnswer = (data) => {
     // data recieved here is { answer }
@@ -120,4 +113,46 @@ export const handleIncomingPreOfferAnswer = (data) => {
         rejected: true,
         reason: rejectionReason
     }));
+    resetCallData();
+};
+
+// Called from react component that monitors the Accept button state
+export const acceptIncomingCallRequest = () => {
+    sendPreOfferAnswer({
+        callerSocketId: connectedUserSocketId, 
+        answer: preOfferAnswers.Accepted
+    });
+}; 
+
+// Called from react component that monitors the Reject button state
+export const rejectIncomingCallRequest = () => {
+    sendPreOfferAnswer({
+        callerSocketId: connectedUserSocketId,
+        answer: preOfferAnswers.Rejected
+    });
+
+    resetCallData();
+}; 
+
+// Peer Connection related methods
+const createPeerConnection = () => {
+    peerConnection = RTCPeerConnection(peerConnectionConfig);
+    
+    const localStream = store.getState().callLocalStream.localStream;
+    
+    // mediaStream objects have multiple tracks associated with it that make up the complete stream (namely, audio & video tracks)
+    for (const track of localStream.getTracks()) {
+        peerConnection.addTrack(track, localStream);
+    }
+
+    // When we receive a stream with tracks from a remote peer
+    peerConnection.ontrack = (event) => {
+        const remoteStream = event.streams[0]; 
+        const remoteTrack = event.track; 
+        // add stream to redux store  
+    };
+
+    peerConnection.onicecandidate = (event) => {
+        // send our ice candidates to other connected users
+    };
 };
